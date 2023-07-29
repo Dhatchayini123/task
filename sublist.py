c57e8baf242c1ffb6b605a6a5253c2db79fe541e
Dhatchayini123 <138356513+Dhatchayini123@users.noreply.github.com>
2023-07-29 03:05:43 -0700

    third file

---
+++ sublist.py
@@ -0,0 +1,7 @@
+list=[[1,2,3],[10,30]]
+list0=sum(list[0])
+list1=sum(list[1])
+if list0> list1:
+    print(f'the {list[0]} is greater than {list[1]}')
+else:
+    print(f'the {list[0]} is lesser than{list[1]}')
